bd421740b01564c0315a35fd86614d7f7a9db511
Trifan Alexandru <trifan.ac@icloud.com>
2023-05-11 11:21:44 +0300

    Connected to OpenWeatherMap API

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 
 const App = () => {
-  return <div>App</div>;
+  const apiKey = import.meta.env.VITE_API_KEY;
+
+  const [data, setData] = useState({});
+  const [location, setLocation] = useState("");
+
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${apiKey}`;
+
+  const searchLocation = (event) => {
+    if (event.key === "Enter") {
+      axios.get(url).then((response) => {
+        setData(response.data);
+        console.log(response.data);
+      });
+      setLocation("");
+    }
+  };
+
+  return (
+    <div>
+      <div>
+        <input
+          value={location}
+          onChange={(event) => setLocation(event.target.value)}
+          onKeyPress={searchLocation}
+          type="text"
+          placeholder="Enter location here"
+        />
+      </div>
+      <div>{data.name}</div>
+    </div>
+  );
 };
 
 export default App;
